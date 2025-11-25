95584070b2b8759005cb50adf0fff412d2cc74bd
Skiyoshika <hiuramika122@gmail.com>
2025-11-24 19:06:00 -0800

    Cleanup: Removed unnecessary build artifacts (target/ folder)

--- target/debug/build/winit-94b1dfd6d993aa91/build_script_build-94b1dfd6d993aa91.d
+++
@@ -1,5 +0,0 @@
-E:\QNMDsol\target\debug\build\winit-94b1dfd6d993aa91\build_script_build-94b1dfd6d993aa91.d: C:\Users\10411\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winit-0.28.7\build.rs
-
-E:\QNMDsol\target\debug\build\winit-94b1dfd6d993aa91\build_script_build-94b1dfd6d993aa91.exe: C:\Users\10411\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winit-0.28.7\build.rs
-
-C:\Users\10411\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winit-0.28.7\build.rs:
